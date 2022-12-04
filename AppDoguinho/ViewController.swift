7c6658c902441b4fd008bfb43f9507135e21eeac
lucas <lucashampell@gmail.com>
2022-12-04 18:06:11 -0300

    Initial Commit

---
+++ AppDoguinho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDoguinho
+//
+//  Created by lucas meireles on 04/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
