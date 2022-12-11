5b18765531918a72c14b35195eba94083f528d3c
lucas <lucashampell@gmail.com>
2022-12-11 19:35:22 -0300

    Adicionando calculo de idade e anos

---
+++ AppDoguinho/MainScreen/DogModel.swift
@@ -0,0 +1,19 @@
+//
+//  DogModel.swift
+//  AppDoguinho
+//
+//  Created by lucas meireles on 11/12/22.
+//
+
+import Foundation
+
+
+class DogModel {
+    var tamanho:String = "Pequeno"
+    var idade:Int = 1
+    
+    init(tamanho: String, idade: Int) {
+        self.tamanho = tamanho
+        self.idade = idade
+    }
+}
